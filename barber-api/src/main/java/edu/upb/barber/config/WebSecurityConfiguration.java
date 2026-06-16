17880a51a7add586a7fdcd91260df6f6213cc055
Jose Antonio <joseluquet1@upb.edu>
2026-06-16 00:56:19 -0400

    Cambio para mostrar Sucursales en Landing Page

--- barber-api/src/main/java/edu/upb/barber/config/WebSecurityConfiguration.java
+++ barber-api/src/main/java/edu/upb/barber/config/WebSecurityConfiguration.java
@@ -50,6 +50,7 @@ public class WebSecurityConfiguration implements WebMvcConfigurer, Serializable 
 //                                        .requestMatchers(HttpMethod.PUT, "/api/v1/empresas/**").permitAll()
                                         .requestMatchers(HttpMethod.POST,"/api/v1/auth/login").permitAll()
                                         .requestMatchers(HttpMethod.POST,"/api/v1/stereum").permitAll()
+                                        .requestMatchers(HttpMethod.GET,"/api/v1/sucursales").permitAll()
 //                                        .requestMatchers(HttpMethod.PUT, "/api/empresas/**").permitAll()
 
                                         .requestMatchers("/api/v2/**").denyAll()
